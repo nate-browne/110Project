2af4fed4ea19bc5c958f7172120eea0353b9b719
Nate Browne <natebrowne@outlook.com>
2019-04-23 16:09:42 -0700

    TempProject works now

---
+++ TempProject/index.android.js
@@ -0,0 +1,10 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import TempProject from './outputs/App';
+import {AppRegistry} from 'react-native'
+
+AppRegistry.registerComponent('TempProject', () => TempProject);
