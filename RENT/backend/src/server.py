70376c033a691b2dc895149af93158fc3824e34f
Nate Browne <natebrowne@outlook.com>
2019-04-30 17:56:16 -0700

    adding object imports to server.py

--- RENT/backend/src/server.py
+++ RENT/backend/src/server.py
@@ -1,6 +1,14 @@
-from db_modules import db
 from passlib.hash import sha256_crypt
 from flask import Flask, request, abort, jsonify, url_for, make_response
+
+from db_modules import db
+from db_modules.db_objects.users import Users
+from db_modules.db_objects.rental import Rental
+from db_modules.db_objects.roommates import Roommates
+from db_modules.db_objects.contact_info import ContactInfo
+from db_modules.db_objects.expenses import ExpenseListItem, Expenses
+from db_modules.db_objects.property_document import PropertyDocument
+from db_modules.db_objects.grocery_list import GroceryList, GroceryListItem
 
 app = Flask(__name__)
 app.secret_key = 'aabjeetGx2LaCC1a4opBUsc95a6KmbKX20hHIq8ie5r8FJx5S9fSTk2hYsz85BLfNxk9vjw'
