0910951ede8e307942ff0db6e8ca5bdda6250a2e
Nate Browne <natebrowne@outlook.com>
2019-05-04 14:39:13 -0700

    the linter no longer wants to kill us

--- RENT/backend/src/database.py
+++ RENT/backend/src/database.py
@@ -5,6 +5,44 @@ from server import app
 
 db = SQLAlchemy(app)
 
+
 class Users(db.Model):
-  id = db.Column(db.Integer, primary_key=True, nullable=False)
-  email = db.Column(db.String(255), unique=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    first_name = db.Column(db.String(255), nullable=False)
+    last_name = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    rental = db.Column(db.Integer, db.ForeignKey('Rental.id'), nullable=True)
+
+    def __repr__(self) -> str:
+        return '<User>\nname: {} {}\nemail: {}\nrentalID: {}'.format(
+            self.first_name, self.last_name, self.email, self.rental)
+
+
+class Roommate(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    roomate1 = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=True,
+                         default=None)
+    roomate2 = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=True,
+                         default=None)
+    roomate3 = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=True,
+                         default=None)
+    roomate4 = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=True,
+                         default=None)
+    roomate5 = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=True,
+                         default=None)
+
+    def __repr__(self) -> str:
+        to_print = []
+        for attr in dir(self):
+            if attr != id and attr is not None:
+                to_print.append(attr)
+                to_print.append("\n")
+        val = []
+        val.append("<RoommateList>\n")
+        val.extend(to_print)
+        return ''.join(val)
+
+
+def getUserByObject(user: Users) -> Optional[Users]:
+    pass
--- RENT/backend/src/server.py
+++ RENT/backend/src/server.py
@@ -1,29 +1,28 @@
-from flask import Flask, request, abort, jsonify, url_for, make_response
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, jsonify
+from database import Users, getUserByObject
 
 # Local database URL for everyone
 DB_URL = 'mysql://root@localhost/rent'
 
 app = Flask(__name__)
-app.secret_key = 'aabjeetGx2LaCC1a4opBUsc95a6KmbKX20hHIq8ie5r8FJx5S9fSTk2hYsz85BLfNxk9vjw'
+app.secret_key = 'aabjeetGx2LaCC1a4opBUsc95a6KmbKX20hHIq8ie5r8FJx5S9fSTk2hYsz8\
+5BLfNxk9vjw'
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 
-from database import *
 
 @app.route('/login', methods=["POST"])
 def login():
-  email = request.json['email']
-  password = request.json['password']
-  
-  '''
-  user = Users(email,password)
-  user = getUserByObject(user)
-  
-  if user is not None:
-    return jsonify(user),200
-  else:
-    return {},204
-  '''
+    email = request.json['email']
+    password = request.json['password']
+
+    user = Users(email, password)
+    user = getUserByObject(user)
+
+    if user is not None:
+        return jsonify(user), 200
+    else:
+        return {}, 204
+
 
 if __name__ == "__main__":
-  app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
