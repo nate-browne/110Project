8d0d1e16499f1fc8c45d01470a4db02ae4acbfc4
Shivani Doshi <s1doshi@ucsd.edu>
2019-05-04 13:49:11 -0700

    server changes for login

--- RENT/backend/src/server.py
+++ RENT/backend/src/server.py
@@ -1,23 +1,28 @@
 from flask import Flask, request, abort, jsonify, url_for, make_response
+from flask_sqlalchemy import SQLAlchemy
 
-from db_modules import db
-from db_modules.db_objects.users import Users
-from db_modules.db_objects.rental import Rental
-from db_modules.db_objects.roommates import Roommates
-from db_modules.db_objects.contact_info import ContactInfo
-from db_modules.db_objects.expenses import ExpenseListItem, Expenses
-from db_modules.db_objects.property_document import PropertyDocument
-from db_modules.db_objects.grocery_list import GroceryList, GroceryListItem
+# Local database URL for everyone
+DB_URL = 'mysql://root@localhost/rent'
 
 app = Flask(__name__)
 app.secret_key = 'aabjeetGx2LaCC1a4opBUsc95a6KmbKX20hHIq8ie5r8FJx5S9fSTk2hYsz85BLfNxk9vjw'
-app.config['SQLALCHEMY_DATABASE_URI'] = db.DB_URL
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+database = SQLAlchemy(app)
 
-database = db.DB(app)
-
-@app.route('/')
-def hello() -> str:
-  return "hello"
+@app.route('/login', methods=["POST"])
+def login():
+  email = request.json['email']
+  password = request.json['password']
+  
+  '''
+  user = Users(email,password)
+  user = getUserByObject(user)
+  
+  if user is not None:
+    return jsonify(user),200
+  else:
+    return {},204
+  '''
 
 if __name__ == "__main__":
   app.run(debug=True, host='0.0.0.0')
