dc3f06e3b2e1be4fcb0e848233836713ad1a5d26
James Cor <jcor@ucsd.edu>
2019-05-11 19:06:59 -0700

    rental main page completed

--- RENT/frontend/src/RentalMain.tsx
+++ RENT/frontend/src/RentalMain.tsx
@@ -1,105 +1,68 @@
-/*import React, { Component } from 'react';
-import { Text, View, StyleSheet, Alert, TouchableHighlight } from 'react-native';
-import Dashboard from 'react-native-dashboard';
- 
-const items = [
-  { name: 'Me', background: '#3498db', icon: 'user' },
-  { name: 'Logistics', background: '#ef0202', icon: 'archive' },
-  { name: 'Grocery List', background: '#efcf02', icon: 'list-alt' },
-  { name: 'Roommates', background: '#02ef1d', icon: 'users' },
-  { name: 'Bulletin Board', background: '#02cbef', icon: 'group' },
-  { name: 'Calendars', background: '#ef5802', icon: 'calendar' },
-];
- 
-export default class App extends Component {
-  _card = (el: { name: string; }) => {
-    switch(el.name){
-      case 'Me':
-        Alert.alert("You pressed me!")
-        break;
-      case 'Logistics':
-        Alert.alert("You pressed logistics!")
-        break;
-      case 'Grocery List':
-        Alert.alert("You pressed grocery list!")
-        break;
-      case 'Roommates':
-        Alert.alert("You pressed roommates!")
-        break;
-      case 'Friends':
-        Alert.alert("You pressed friends!")
-        break;
-      case 'Calendars':
-        Alert.alert("You pressed calendars!")
-        break;
-      default:
-        Alert.alert("This should not happen")
-    }
-  };
-  render() {
-    return (
-      <View style={styles.container}>
-        <Dashboard items={items} background={true} card={this._card} column={2} />
-      </View>
-    );
-  }
-}
- 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ecf0f1',
-  },
-});
-
-*/
-
 import React, { Component } from 'react';
-import styles from './style/RentalMain-Stylesheet'; // This is how you can import stuff from other folders
-import { Text, View, StyleSheet, Alert, TouchableHighlight } from 'react-native';
-import { Button } from 'react-native-elements';
+import { Vibration, Alert, View, Text, TouchableHighlight } from 'react-native';
+import { FlatGrid } from 'react-native-super-grid';
+import { Icon } from 'react-native-elements';
+import styles from './style/RentalMain-Stylesheet';
 
-export default class App extends Component {
+export default class Example extends Component {
   render() {
+    const items = [
+      { name: 'Contact Info', code: '#1abc9c', icon:'face' }, 
+      { name: 'Leasing Info', code: '#2ecc71', icon:'info' },
+      { name: 'Grocery List', code: '#3498db', icon:'assignment' }, 
+      { name: 'Roommates', code: '#9b59b6', icon:'group' },
+      { name: 'Bulletin Board', code: '#34495e', icon:'mode-edit' }, 
+      { name: 'Calendar', code: '#16a085', icon:'insert-invitation' },
+      { name: 'Expenses', code: '#16a085', icon:'attach-money' },
+    ];
+
     return (
-      <View style={styles.container}>
-        <View style={styles.button}>
-          <Button
-            raised={true}
-            title="Button 1"
-            onPress={() =>{
-              Alert.alert("Button 1");
-            }}
-          />
-        </View>
-        <View style={styles.button}>
-          <Button
-            raised={true}
-            title="Button 1"
-            onPress={() =>{
-              Alert.alert("Button 1");
-            }}
-          />
-        </View>
-        <View style={styles.button}>
-          <Button
-            raised={true}
-            title="Button 1"
-            onPress={() =>{
-              Alert.alert("Button 1");
-            }}
-          />
-        </View>
-        <View style={styles.button}>
-          <Button
-            raised={true}
-            title="Button 1"
-            onPress={() =>{
-              Alert.alert("Button 1");
-            }}
-          />
-        </View>
-      </View>
+      <FlatGrid
+        itemDimension={130}
+        items={items}
+        style={styles.gridView}
+        // staticDimension={300}
+        // fixed
+        // spacing={20}
+        renderItem={({ item, index }) => (
+          //<View style={[styles.itemContainer, { backgroundColor: item.code }]}>
+            <TouchableHighlight onPress={()=> {
+              Vibration.vibrate(30)
+              switch(item.name){
+                case 'Contact Info':
+                  Alert.alert("You pressed personal info!")
+                  break;
+                case 'Leasing Info':
+                  Alert.alert("You pressed leasing info!")
+                  break;
+                case 'Grocery List':
+                  Alert.alert("You pressed grocery list!")
+                  break;
+                case 'Roommates':
+                  Alert.alert("You pressed roommates!")
+                  break;
+                case 'Bulletin Board':
+                  Alert.alert("You pressed bulletin board!")
+                  break;
+                case 'Calendar':
+                  Alert.alert("You pressed calendar!")
+                  break;
+                case 'Expenses':
+                  Alert.alert("You pressed expenses!")
+                  break;
+                default:
+                  Alert.alert("This should not happen")
+              }
+            }} 
+            activeOpacity= {0.5} style={[styles.itemContainer, { backgroundColor: item.code }]}>
+              <View>
+                <Icon name={item.icon} color='white' size={49}/>
+                <Text style={styles.itemName}>{item.name}</Text> 
+              </View> 
+            </TouchableHighlight>
+          //</View>
+        )}
+      />
     );
   }
 }
--- RENT/frontend/src/style/RentalMain-Stylesheet.ts
+++ RENT/frontend/src/style/RentalMain-Stylesheet.ts
@@ -1,34 +1,28 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 36,
-    color: 'white',
-    alignItems: 'stretch',
-    textAlign: 'center',
-    padding: 20,
-  },
-  background: {
-    width: '100%', 
-    height: '100%', 
-    alignItems: 'center', 
-    justifyContent: 'center',
-  },
-  image: {
-    width: 150,
-    height: 150,
-    alignItems: 'stretch',
-  },
-  button: {
-    width: '30%',
-    margin: 10
-  },
-});
+    gridView: {
+      marginTop: 20,
+      flex: 1,
+    },
+    itemContainer: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 5,
+      padding: 10,
+      height: 250,
+    },
+    itemName: {
+      fontSize: 16,
+      alignItems: 'center',
+      color: '#fff',
+      fontWeight: '600',
+    },
+    itemCode: {
+      fontWeight: '600',
+      fontSize: 12,
+      color: '#fff',
+    },
+  });
 
-export default styles;
+  export default styles;
