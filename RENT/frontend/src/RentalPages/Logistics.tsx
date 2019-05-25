599b75a7ab4abfc65f3729569cc81025cc3d8af7
tmok825 <tamok@ucsd.edu>
2019-05-25 16:00:37 -0700

    changed logistics stuff

--- RENT/frontend/src/RentalPages/Logistics.tsx
+++ RENT/frontend/src/RentalPages/Logistics.tsx
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
 import { Alert, ScrollView, Text, Image, TouchableOpacity, ImageBackground } from 'react-native';
+
 import CountDown from 'react-native-countdown-component';
 import styles from '../style/Logistics-Stylesheet';
+import {Divider, Overlay} from "react-native-elements";
 
 const endDate = 25; // Number of days away
 
 export default class Logistics extends Component {
+  state={
+    image1Visible: false,
+  }
 
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: "8875 Costa Verde, La Jolla",
+      headerStyle: {
+        backgroundColor: '#2bc0cd',
+
+      },
+
+      headerBackTitle: "Rental Home",
+      headerTitleStyle: {
+        fontWeight: 'bold',
+        color: "white",
+      },
+      headerTintColor: "white",
+
+    };
+  };
   render() {
     return (
-      <ImageBackground imageStyle={{opacity: 0.4}} source={{uri: 'https://img.heypik.com/background-image/20190122/psd-beautiful-blue-dreamy-starry-watercolor-background-heypik-7WU4487.jpg?x-oss-process=image/quality,q_70/watermark,image_c2h1aXlpbl9uZXcucG5n,g_center'}} style={styles.background}>
+      <ImageBackground imageStyle={{opacity: 0.4}} style={styles.background}>
         <ScrollView style={styles.container}>
-          <Text style={styles.countdownHeader}> Address: </Text>
-          <Text style={styles.address1}> 8875 Costa Verde Blvd Apt.807 </Text>
-          <Text style={styles.address2}> La Jolla, CA 92210 </Text>
-          <Text style={styles.countdownHeader}> Lease Ends In: </Text>
+
+          <Text style={styles.countdownHeader}> Lease ends in: </Text>
           <CountDown
               until={24 * 60 * 60 * endDate}
               size={30}
@@ -23,52 +43,64 @@ export default class Logistics extends Component {
               digitTxtStyle={{color: '#555555'}}
               timeLabelStyle={{color: '#555555', fontWeight: 'bold'}}
               timeToShow={['D']}
-              timeLabels={{d: 'Days left',}}
+              timeLabels={{d: 'Days',}}
               style={{padding: 10}}
           />
-          <Text style={styles.lease}> Lease ends on: 05/12/19 </Text>
-          <Text style={styles.countdownHeader}> Images: </Text>
-          <ScrollView horizontal={true}>
-            <TouchableOpacity onPress={() => { Alert.alert("Make me bigger") }}>
-              <Image style={{margin: 4, width: 225, height: 150}} source={{uri: 'http://cdn.home-designing.com/wp-content/uploads/2014/06/marble-finish-room-divider.jpeg'}}/>
+          <Text style={styles.lease}>  (Ends 05/12/19) </Text>
+
+          <Text> </Text>
+          <Text> </Text>
+          <Divider style={{ backgroundColor: '#2bc0cd', height: 10}} />
+          
+
+          <Text style={styles.otherHeader}> Images: </Text>
+          <ScrollView style={styles.imageScroll} horizontal={true} minimumZoomScale={1} maximumZoomScale={5}>
+            <TouchableOpacity onPress={() => { <Image style={styles.image} source = {{uri: 'http://cdn.home-designing.com/wp-content/uploads/2014/06/marble-finish-room-divider.jpeg'}}/> }}>
+              <Image style={{margin: 4, width: 225, height: 150, borderColor: "#000000", borderWidth:10}} source={{uri: 'http://cdn.home-designing.com/wp-content/uploads/2014/06/marble-finish-room-divider.jpeg'}}/>
             </TouchableOpacity>
 
             <TouchableOpacity onPress={() => { Alert.alert("Make me bigger") }}>
-              <Image style={{margin: 4, width: 225, height: 150}} source={{uri: 'https://cdn.interiorzine.com/wp-content/uploads/2017/11/interior-design-for-small-studio-apartment.jpg'}}/>
+              <Image style={{margin: 4, width: 225, height: 150, borderColor: "#000000", borderWidth:10}} source={{uri: 'https://cdn.interiorzine.com/wp-content/uploads/2017/11/interior-design-for-small-studio-apartment.jpg'}}/>
             </TouchableOpacity>
 
             <TouchableOpacity onPress={() => { Alert.alert("Make me bigger") }}>
-              <Image style={{margin: 4, width: 225, height: 150}} source={{uri: 'https://cdn.interiorzine.com/wp-content/uploads/2017/11/modern-studio-apartment-interior-design-ideas-hang-things.jpg'}}/>
+              <Image style={{margin: 4, width: 225, height: 150, borderColor: "#000000", borderWidth:10}} source={{uri: 'https://cdn.interiorzine.com/wp-content/uploads/2017/11/modern-studio-apartment-interior-design-ideas-hang-things.jpg'}}/>
             </TouchableOpacity>
 
             <TouchableOpacity onPress={() => { Alert.alert("Make me bigger") }}>
-              <Image style={{margin: 4, width: 225, height: 150}} source={{uri: 'https://cdn.interiorzine.com/wp-content/uploads/2017/11/tiny-apartment-design-ideas-folding-glass-walls.jpg'}}/>
+              <Image style={{margin: 4, width: 225, height: 150, borderColor: "#000000", borderWidth:10}} source={{uri: 'https://cdn.interiorzine.com/wp-content/uploads/2017/11/tiny-apartment-design-ideas-folding-glass-walls.jpg'}}/>
             </TouchableOpacity>
 
             <TouchableOpacity onPress={() => { Alert.alert("Make me bigger") }}>
-              <Image style={{margin: 4, width: 225, height: 150}} source={{uri: 'http://davescosmicsubsatl.com/wp-content/uploads/parser/one-bedroom-apartment-interior-design-1.jpg'}}/>
+              <Image style={{margin: 4, width: 225, height: 150, borderColor: "#000000", borderWidth:10}} source={{uri: 'http://davescosmicsubsatl.com/wp-content/uploads/parser/one-bedroom-apartment-interior-design-1.jpg'}}/>
             </TouchableOpacity>
 
           </ScrollView>
 
-          <Text style={styles.countdownHeader}> Contract: </Text>
-          <ScrollView horizontal={true}>
+          <Text> </Text>
+          <Text> </Text>
+          <Divider style={{ backgroundColor: '#2bc0cd', height: 10}} />
+
+
+          <Text style={styles.otherHeader}> Contract: </Text>
+          <ScrollView style={styles.imageScroll} horizontal={true}>
             <TouchableOpacity onPress={() => { Alert.alert("Make me bigger") }}>
-              <Image style={{margin: 4, width: 160, height: 220}} source={{uri: 'https://images.template.net/wp-content/uploads/2017/05/House-Lease-Contract-Template.jpg'}}/>
+              <Image style={{margin: 4, width: 160, height: 220, borderColor: "#000000", borderWidth:5}} source={{uri: 'https://images.template.net/wp-content/uploads/2017/05/House-Lease-Contract-Template.jpg'}}/>
             </TouchableOpacity>
             <TouchableOpacity onPress={() => { Alert.alert("Make me bigger") }}>
-              <Image style={{margin: 4, width: 160, height: 220}} source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYj2Oswq-uLmtJpYAs-oAPaaF9dqvUUr2KCQxZamLyLM8Uz5oxjw'}}/>
+              <Image style={{margin: 4, width: 160, height: 220, borderColor: "#000000", borderWidth:5}} source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYj2Oswq-uLmtJpYAs-oAPaaF9dqvUUr2KCQxZamLyLM8Uz5oxjw'}}/>
             </TouchableOpacity>
             <TouchableOpacity onPress={() => { Alert.alert("Make me bigger") }}>
-              <Image style={{margin: 4, width: 160, height: 220}} source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYj2Oswq-uLmtJpYAs-oAPaaF9dqvUUr2KCQxZamLyLM8Uz5oxjw'}}/>
+              <Image style={{margin: 4, width: 160, height: 220, borderColor: "#000000", borderWidth:5}} source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYj2Oswq-uLmtJpYAs-oAPaaF9dqvUUr2KCQxZamLyLM8Uz5oxjw'}}/>
             </TouchableOpacity>
             <TouchableOpacity onPress={() => { Alert.alert("Make me bigger") }}>
-              <Image style={{margin: 4, width: 160, height: 220}} source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYj2Oswq-uLmtJpYAs-oAPaaF9dqvUUr2KCQxZamLyLM8Uz5oxjw'}}/>
+              <Image style={{margin: 4, width: 160, height: 220, borderColor: "#000000", borderWidth:5}} source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYj2Oswq-uLmtJpYAs-oAPaaF9dqvUUr2KCQxZamLyLM8Uz5oxjw'}}/>
             </TouchableOpacity>
             <TouchableOpacity onPress={() => { Alert.alert("Make me bigger") }}>
-              <Image style={{margin: 4, width: 160, height: 220}} source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYj2Oswq-uLmtJpYAs-oAPaaF9dqvUUr2KCQxZamLyLM8Uz5oxjw'}}/>
+              <Image style={{margin: 4, width: 160, height: 220, borderColor: "#000000", borderWidth:5}} source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYj2Oswq-uLmtJpYAs-oAPaaF9dqvUUr2KCQxZamLyLM8Uz5oxjw'}}/>
             </TouchableOpacity>
           </ScrollView>
+          <Text style={styles.countdownHeader}></Text>
         </ScrollView>
       </ImageBackground>
     );
