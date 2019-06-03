cd5ead8581040afcd1958aeb622385a655cfc84e
Akshay <kamal.akshay@att.net>
2019-06-02 21:14:19 -0700

    Added skeleton for calendar event modification

---
+++ RENT/frontend/src/style/Calendar-Stylesheet.ts
@@ -0,0 +1,33 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+
+  addEventForm: {
+    backgroundColor: '#EC4E20',
+  },
+
+  dateTimeGroup: {
+    display:'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 2,
+    marginBottom: 2,
+  },
+
+  dateTimeButton: {
+    width: '30%',
+    marginLeft: 2,
+    marginRight: 2,
+    borderColor: '#000',
+    borderRadius: 3,
+  },
+
+  dateTimeTitle: {
+    width: '30%',
+    marginLeft: 2,
+    marginRight: 2,
+  }
+});
+
+export default styles;
